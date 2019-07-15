2a42ea334c7fadf7c0db9562cade603afadaec82
John Pentakalos <jxp9pd@virginia.edu>
2019-07-14 23:36:48 -0400

    Reconfigured poster preprocessing to run with command line arguments. Also reconfigured so it runs to accomodate existing directories and copied movie posters

--- poster_preprocess.py
+++ poster_preprocess.py
@@ -10,17 +10,33 @@ def prep_images(df, data_dir, data_class):
     for genre in genre_list:
         path = data_dir + data_class
         new_dir = os.path.join(path, genre)
-        os.mkdir(new_dir)
+        movie_copy = set()
+        if os.path.isdir(new_dir):
+            #If folder already exists, only include new posters
+            print('Folder ' + genre + ' already exists') 
+            a = df.loc[df[genre] == 1]['id'] + '.jpg'
+            b = pd.Series(os.listdir(new_dir))
+            full = pd.concat([a,b])
+            movie_copy = full.drop_duplicates(keep=False)
+            print ('Posters existing: ' + str(len(b)))
+            print ('Posters added: ' + str(len(movie_copy)))
+        else:
+            os.mkdir(new_dir)
+            movie_copy = df.loc[df[genre] == 1]['id'] + '.jpg'
         file_path = path + genre + '/'
-        movies_src = data_dir + 'poster_imgs/' + df.loc[df[genre] == 1]['id'] + '.jpg'
-        movies_dest = file_path + df.loc[df[genre] == 1]['id'] + '.jpg'
+        movies_src = data_dir + 'poster_imgs/' + movie_copy
+        movies_dest = file_path + movie_copy
         movies = pd.DataFrame.from_dict({'src': movies_src, 'dest':movies_dest})
         movies.apply(lambda x: shutil.copyfile(x['src'], x['dest']), axis=1)
-        print(genre + ' had ' + str(len(movies)) + ' posters.')
+        print(genre + ' has ' + str(len(os.listdir(new_dir))) + ' posters.')
 
 #%%
-data_path = sys.argv[0]
-train_path = data_path + 'train/'
-Y = pd.read_csv(data_path + 'genres.csv')
+if __name__ == ("__main__"):
+    print(sys.argv[1])
+    data_path = sys.argv[1]
+    train_path = data_path + 'train/'
+    Y = pd.read_csv(data_path + 'genres.csv')
+    test_data = Y[:400]
+    prep_images(test_data, data_path, 'train/')
 #%%
-test_data = Y[:100]
+
--- resnet_posters.py
+++ resnet_posters.py
@@ -8,60 +8,30 @@ from keras.models import Model
 from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
 
+# Import Tensorflow with multiprocessing
 if K.backend() == 'tensorflow':
     K.set_image_dim_ordering("tf")
- 
-# Import Tensorflow with multiprocessing
 mpl.rcParams['figure.figsize'] = (16.0, 8.0) 
-
+#%%
+'''Load in the pre-trained Resnet50 model'''
 model = resnet50.ResNet50(weights='imagenet', include_top=False, input_shape=(268, 182, 3))
 model.summary()
 x = model.output
 x = GlobalAveragePooling2D()(x)
 predictions = Dense(26, activation='sigmoid')(x)
 genre_model = Model(inputs=model.input, outputs=predictions)
-#genre_model.compile(loss='binary_crossentropy', optimizer='sgd')
-#genre_model.compile(optimizer='sgd')
 genre_model.compile(loss="binary_crossentropy", optimizer='sgd', metrics=["accuracy"])
 #Freeze all but the last layer
 for layer in genre_model.layers[:-1]:
     layer.trainable = False
 
 #%%
-#
-# In this section, we distribute the images across three directories, 
-# one for training, one for validation and one for testing.
-#
-#load in X and Y
-data_path = '/Users/johnpentakalos/Posters/'
-train_path = data_path + 'train/'
-Y = pd.read_csv(data_path + 'genres.csv')
-#%%
-def prep_images(df, data_dir, data_class):
-    '''splits the data into a different directory per genre.'''
-    genre_list = df.columns.tolist()
-    genre_list.remove('id')
-    for genre in genre_list:
-        path = data_dir + data_class
-        new_dir = os.path.join(path, genre)
-        os.mkdir(new_dir)
-        file_path = path + genre + '/'
-        movies_src = data_dir + 'poster_imgs/' + df.loc[df[genre] == 1]['id'] + '.jpg'
-        movies_dest = file_path + df.loc[df[genre] == 1]['id'] + '.jpg'
-        movies = pd.DataFrame.from_dict({'src': movies_src, 'dest':movies_dest})
-        movies.apply(lambda x: shutil.copyfile(x['src'], x['dest']), axis=1)
-        print(genre + ' had ' + str(len(movies)) + ' posters.')
-#%%
-#Only run the one time.
-train_data = Y[:200]
-prep_images(train_data, data_path, 'train/')
-#%%
 train_datagen = ImageDataGenerator(rescale=1./255)
 #validation_datagen = ImageDataGenerator(rescale=1./255)
 #test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(train_path,
-    target_size=(268, 182), batch_size=16, class_mode=None)
+    target_size=(268, 182), batch_size=32, class_mode=None)
 
 #for data_batch, labels_batch in train_generator:
 #    print('data batch shape:', data_batch.shape)
